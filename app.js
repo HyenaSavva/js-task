af21a110684cc5cdbae1b98db3e15989a10f5ad1
HyenaSavva <re4meister@gmai.com>
2021-04-07 21:47:14 +0300

    resolve task

---
+++ app.js
@@ -0,0 +1,34 @@
+let n = 3
+
+switch (n) {
+    case 0 :
+        console.log('zero')
+        break
+    case 1 :
+        console.log('odin')
+        break
+    case 2 :
+        console.log('dwa')
+        break
+    case 3 :
+        console.log('tri')
+        break
+    case 4 :
+        console.log('cetire')
+        break
+    case 5 :
+        console.log('peat')
+        break
+    case 6 :
+        console.log('shest')
+        break
+    case 7 :
+        console.log('sem')
+        break
+    case 8 :
+        console.log('vosem')
+        break
+    case 9 :
+        console.log('NINE')
+        break
+}
