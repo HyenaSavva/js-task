b2618bce521967664c94a8a9c080ac03a7133460
HyenaSavva <re4meister@gmai.com>
2021-04-08 11:37:41 +0300

    used sort() , task 3

---
+++ BASIC/3/app.js
@@ -0,0 +1,21 @@
+let a  = [ 5, 2, 3, 3, 4, 5, 0]
+
+a.sort()
+
+console.log(a)
+// const compare = (a)=> {  a < a[a.length] ? a.reduce( ) :
+//     let i = 0
+//     let cont
+//     a.forEach(()=>{
+//         if (a[i] < a[i++]){
+//             cont = a[i]
+//             a[i]=a[i++]
+//             a[i++]=cont
+//         }
+//         console.log(a[i])
+//         i++
+//     })
+//     return a
+// }
+//
+// console.log(compare(a))
