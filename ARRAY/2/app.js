ef79be99ab503cb0010e02e1b663016e12a3ce19
HyenaSavva <re4meister@gmai.com>
2021-04-08 17:05:06 +0300

    add array of obj , try to use reduce() , nothing works

---
+++ ARRAY/2/app.js
@@ -0,0 +1,28 @@
+const person =[
+    {
+        name: 'Alex',
+        marks: [8, 10, 7, 5, 4]
+    },
+    {
+        name: 'Dima',
+        marks: [9, 9, 3, 6, 7]
+    },
+    {
+        name: 'Jora',
+        marks: [4, 12, 1, 2, 6]
+    },
+    {
+        name: 'Vasea',
+        marks: [8, 1, 4, 8, 5]
+    }
+]
+
+const avgMarks = (person) =>{
+    // const sum = this.marks.reduce((acc , mark)=>{
+    //a
+    // } )
+    // console.log(`Name ${person.name} ` + `with avg marks ` )
+    console.log(person.name)
+}
+
+console.log(avgMarks(person))
