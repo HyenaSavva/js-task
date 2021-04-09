921f31bdcf16b81cd8eb53dee93215cd46ee9e11
HyenaSavva <re4meister@gmai.com>
2021-04-09 14:24:27 +0300

    was added functions: avgMarks, personWithAvgMarks, showPerson

--- ARRAY/2/app.js
+++ ARRAY/2/app.js
@@ -1,28 +1,59 @@
 const person =[
     {
         name: 'Alex',
-        marks: [8, 10, 7, 5, 4]
+        marks: [0, 1, 2, 3, 4]
     },
     {
         name: 'Dima',
-        marks: [9, 9, 3, 6, 7]
+        marks: [9, 9, 8, 6, 7]
     },
     {
         name: 'Jora',
-        marks: [4, 12, 1, 2, 6]
+        marks: [2, 2, 7, 2, 6]
     },
     {
         name: 'Vasea',
-        marks: [8, 1, 4, 8, 5]
+        marks: [8, 9, 5, 8, 5]
     }
 ]
-
-const avgMarks = (person) =>{
-    // const sum = this.marks.reduce((acc , mark)=>{
-    //a
-    // } )
-    // console.log(`Name ${person.name} ` + `with avg marks ` )
-    console.log(person.name)
+const avgMarks = student => {
+        return student.marks.reduce((acc, mark) => acc + mark) / student.marks.length
 }
 
-console.log(avgMarks(person))
+const personWithAvgMarks = person => {
+    const students = []
+
+    person.forEach((student) => {
+        students.push({ name:student.name , mark:avgMarks(student) })
+    })
+    return students
+}
+
+const showPerson = person => {
+    person.forEach((student , i) => {
+        console.log(`Name: ${student.name} ` + `
+        avg marks ${student.mark}`)
+    })
+}
+
+// const lessFive = person => {
+//     person.forEach((student , i)=>{
+//         if (2 < 5){
+//             console.log(`${student.name} `)
+//         }
+//     })
+// }
+//
+// const maxAndMin = person => {
+//     // getAvgMarks(person).sort((a ,b) => { a.marks - b.marks})
+// }
+//
+console.log(`Students name's with avg marks:`)
+showPerson(personWithAvgMarks(person))
+//
+console.log(`Students with mark lesser then 5:`)
+// lessFive(personWithAvgMarks(person))
+//
+// console.log(`Students with max mark and minim mark :`)
+// maxAndMin(person)
+//
